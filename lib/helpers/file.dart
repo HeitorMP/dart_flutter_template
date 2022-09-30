b7d373f2c378f8faacbadbb2178d00ffa0c713ff
heitormp <heitormp@protonmail.com>
2022-09-30 15:17:10 +0100

    start point

---
+++ lib/helpers/file.dart
@@ -0,0 +1,19 @@
+Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/task.json}");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String?> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
